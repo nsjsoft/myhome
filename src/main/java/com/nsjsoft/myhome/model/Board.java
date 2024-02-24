7428ac53b87a06e114d874eecc93218deb575dba
나상진 Mac <nsj0409@gmail.com>
2024-02-24 12:56:47 +0900

    Mac Book Commit

--- src/main/java/com/nsjsoft/myhome/model/Board.java
+++ src/main/java/com/nsjsoft/myhome/model/Board.java
@@ -18,6 +18,7 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     @NotNull
     @Size(min=2, max=30)
     private String title;
